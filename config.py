cc97ab5320cb3eb4ff59915377a50a02af416819
niara100 <49889352+niara100@users.noreply.github.com>
2020-05-08 22:08:02 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+token = ''
+status = 'Mention me to start chatting' #playing status
+botID = 'b0dafd24ee35a477'
+mentionReply = 'true' #if false the bot wont mention a member when it replies to them
+welcomesEnabled = 'false' #this will make it so the bot will welcome new members
+welcomeChannel =  #the channel where the bot will welcome new members
+welcome_messages = ['welcome',
+                    'welcome i hope you enjoy your stay',
+                    'hi i hope you like the server',
+                    'hi welcome if you wanna talk to me just mention me',
+                    'welcome cutie pie']
+
+error_message = ['yeah i have no idea what that means',
+                     'idk what that means try again',
+                     'YOU IDIOT WHAT DOES THAT MEAN',
+                     'is that even english?',
+                     'idk what that means leave me alone!',
+                     'speak clearly',
+                     'idk what to tell you chief',
+                     'i hate you sometimes',
+                     'why are you like this?',
+                     'uhhh try again please',
+                     'GET OFF MY LAWN'
+                 ]
